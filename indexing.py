77ff691b8bafa40d4a7abec40ee9e7093661e00d
Awa Danfa <31821970+AwaDanfa@users.noreply.github.com>
2023-02-19 11:01:47 +0000

    Add initial file

---
+++ indexing.py
@@ -0,0 +1,30 @@
+""" Write a program that asks a user to enter a 6-letter word then print the word backwards.
+ """
+word = "Hello"
+print("Example 2: ")
+
+# Indexing from 0 to 4
+char1 = word[0]
+char2 = word[1]
+char3 = word[2]
+char4 = word[3]
+char5 = word[4]
+print(char1)
+print(char2)
+print(char3)
+print(char4)
+print(char5)
+
+print("")
+
+# Indexing from -1 to -5
+char1 = word[-1]
+char2 = word[-2]
+char3 = word[-3]
+char4 = word[-4]
+char5 = word[-5]
+print(char1)
+print(char2)
+print(char3)
+print(char4)
+print(char5)
